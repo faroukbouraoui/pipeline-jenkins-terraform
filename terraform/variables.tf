b07511fb100a4571b239bbd20220c0d3bea6e977
farouk <49481558+faroukbouraoui@users.noreply.github.com>
2023-09-01 14:32:09 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,13 @@
 variable "AWS_SECRET_KEY" {
-    default = "odo3iq/p4AH693z/Ij72t0I5vGuhHqS1WESkQX9a" 
+    default = "" 
 }
 
 variable "AWS_ACCESS_KEY" {
-    default = "ASIA3ZFTKHXMXLDQ3CXM" 
+    default = "" 
 }
 
 variable "AWS_SESSION_TOKEN" {
-    default = "FwoGZXIvYXdzEAsaDCaB8D/DSXQczvDqESLAAUApUAu4a0l7xnGnxc2A/zKwHR9JJSqj1mZy+RD5KMY9IRT9nT0aCvmwJjNDu9wiFaAZ8vRLOKQeJaA4H9wZP/6GwslCWfSamvXXcIm3bl6Sfv0z9CbJL0XCZlQVZrwoU95UXlC3U/TmdrE+fzRCe20MemFYzeUVV5gEt0zFq4XRBWkpJGky+Mhsz0kgIIWVedmEH806YAQ/OkuLwbflR4WTsaRklG2KPKN1lG4a6vKw3vTXKHHV7WA5ShcucUkUkijU872nBjItlxLZrjTuipAMGuvwsXBsPc7JVRlOmTaORTMHV7ozIuG6U8bkwsgglURQHZZu" 
+    default = "" 
 }
 
 variable "region" {
